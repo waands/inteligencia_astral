c02a386f74437df7c581a5686a61e584dd19706c
waands <wanderjunior36@gmail.com>
2023-09-24 22:20:04 -0300

    trash removed from main

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,9 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //home: CriarGrupo()
         home: Login());
   }
-  //_PrimeiraTelaState createState() => _PrimeiraTelaState();
 }
-/*
-class _PrimeiraTelaState extends State<PrimeiraTela> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Primeira Tela"),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            ElevatedButton(
-              child: Text("Ir para a segunda tela"),
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  "/login",
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
+
 
 
