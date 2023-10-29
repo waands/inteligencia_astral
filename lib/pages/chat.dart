1a3eff636cdbc1dc4c3ecc9f2a7a418abadf5ccb
mifegui <mife@mifegui.xyz>
2023-10-29 17:29:33 -0300

    openai/chatgpt integration
    (stream doesnt work)

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -3,7 +3,8 @@ import 'package:inteligencia_astral/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:inteligencia_astral/components/navigation_menu.dart';
 import 'package:inteligencia_astral/theme.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Chat extends StatefulWidget {
   @override
@@ -17,18 +18,59 @@ class _ChatState extends State<Chat> {
     lastName: 'Leclerc',
   );
 
-  List<ChatMessage> messages = <ChatMessage>[
-    ChatMessage(
-      text: 'Olá!',
-      user: ChatUser(
-        id: '1',
-        firstName: 'Gustavo',
-        lastName: 'Lozerno',
-      ),
-      createdAt: DateTime.now(),
-      
-    ),
-  ];
+  List<ChatMessage> messages = <ChatMessage>[];
+
+  Future<void> sendMessage(ChatMessage message) async {
+    final apiUrl = "https://esoteria.vercel.app/api/1/chat";
+    List<Map<String, dynamic>> messageList = [];
+
+    for (var m in messages) {
+      if (m.user.id == user.id) {
+        messageList.insert(0, {"role": "user", "content": m.text});
+      } else {
+        messageList.insert(0, {"role": "assistant", "content": m.text});
+      }
+    }
+
+    try {
+      final request = http.Request('POST', Uri.parse(apiUrl))
+        ..headers['Content-Type'] = 'application/json'
+        ..body = jsonEncode({"messages": messageList});
+
+      final streamedResponse = await request.send();
+
+      // Add a placeholder message to be updated in real-time
+      ChatMessage placeholderMessage = ChatMessage(
+        text: "",
+        user: ChatUser(
+          id: '2', // different ID for assistant
+          firstName: 'Assistant',
+          lastName: '',
+        ),
+        createdAt: DateTime.now(),
+      );
+      setState(() {
+        messages.insert(0, placeholderMessage);
+      });
+
+      // Update the placeholder message word by word as chunks come in
+      // TODO: (not working)
+      streamedResponse.stream.transform(utf8.decoder).listen((value) {
+        print(value);
+        setState(() {
+          placeholderMessage.text += value + " ";
+        });
+      }, onError: (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Stream error: $error")),
+        );
+      });
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Exception occurred: $error")),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,17 +83,16 @@ class _ChatState extends State<Chat> {
       body: DashChat(
         currentUser: user,
         messageOptions: MessageOptions(
-          // Customize message and user container colors here
           currentUserContainerColor: AppTheme.colors.purpura,
           currentUserTextColor: Colors.white,
           containerColor: AppTheme.colors.green,
           textColor: Colors.white,
         ),
-
         onSend: (ChatMessage m) {
           setState(() {
             messages.insert(0, m);
           });
+          sendMessage(m);
         },
         messages: messages,
       ),
